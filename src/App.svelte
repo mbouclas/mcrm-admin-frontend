157a86312e92a176fa579c0bad29e1350e117dce
borealia <lucas.jiang.itdev@gmail.com>
2022-08-19 07:12:53 -0400

    Added menus initially

--- src/App.svelte
+++ src/App.svelte
@@ -2,25 +2,17 @@
   import { Router, Route, Link } from "svelte-navigator";
   import Login from "./lib/Auth/Login.svelte";
   import PrivateRoute from "./lib/Auth/PrivateRoute.svelte";
-  import DefaultLayout from './lib/layouts/default.svelte'
+  import DefaultLayout from "./lib/layouts/default.svelte";
 
   import { user } from "./lib/stores";
-
-
 </script>
 
 <Router>
+  <Route path="login">
+    <Login />
+  </Route>
 
-<Route path="login">
-  <Login />
-</Route>
-
-
-
-<PrivateRoute path="/*" let:location>
-<DefaultLayout />
-
-</PrivateRoute>
-
-
+  <PrivateRoute path="/*" let:location>
+    <DefaultLayout />
+  </PrivateRoute>
 </Router>
--- src/lib/Shared/Header.svelte
+++ src/lib/Shared/Header.svelte
@@ -1,23 +1,122 @@
 <script>
-    import {user} from "../stores.ts";
-    import {Link} from "svelte-navigator";
+  import { Link } from "svelte-navigator";
+  import {
+    SidebarGroup,
+    Sidebar,
+    SidebarWrapper,
+    SidebarDropdownItem,
+    SidebarDropdownWrapper,
+    SidebarItem,
+  } from "flowbite-svelte";
 
-    function handleLogout() {
-        $user = null;
-        localStorage.clear();
-    }
+  import {
+    ChartPie,
+    ShoppingCart,
+    ViewGrid,
+    InboxIn,
+    User,
+    Login,
+    Cog,
+    Logout,
+  } from "svelte-heros";
 
+  import { user } from "../stores";
+
+  function handleLogout(e) {
+    e.preventDefault();
+    $user = null;
+    localStorage.clear();
+  }
+
+  let site = {
+    name: "Flowbite-Svelte",
+    href: "/",
+    img: "/images/flowbite-svelte-icon-logo.svg",
+  };
+
+  let spanClass = "flex-1 ml-3 whitespace-nowrap";
 </script>
 
-<header>
-    <nav>
-        <Link to="/">Home</Link>
-        <Link to="about">About</Link>
-        <Link to="profile">Profile</Link>
+<Sidebar class="h-screen sidebar bg-[#2a3042]">
+  <div class="flex justify-center">
+    <Link to="/" class="nav-brand  py-5 px-10">
+      <img
+        class="w-24 object-fit"
+        src="http://skote-v-dark.svelte.themesbrand.com/assets/images/logo-light.png"
+        alt=""
+        srcset=""
+      />
+    </Link>
+  </div>
+  <SidebarWrapper
+    class="rounded-none bg-[#2a3042] text-[#a6b0cf]  hover:bg-[#2a3042] hover:text-white"
+  >
+    <SidebarGroup>
+      <SidebarDropdownWrapper
+        class="text-[#a6b0cf] hover:bg-[#2a3042] hover:text-white"
+        label="Catalogue"
+        icon={{ name: ShoppingCart }}
+      >
+        <SidebarDropdownItem
+          class="text-[#a6b0cf]  hover:bg-[#2a3042] hover:text-white"
+          label="Products"
+        />
+        <SidebarDropdownItem
+          class="text-[#a6b0cf]  hover:bg-[#2a3042] hover:text-white"
+          label="Categories"
+        />
+        <SidebarDropdownItem
+          class="text-[#a6b0cf]  hover:bg-[#2a3042] hover:text-white"
+          label="Properties"
+        />
+        <SidebarDropdownItem
+          class="text-[#a6b0cf]  hover:bg-[#2a3042] hover:text-white"
+          label="Manufacturers"
+        />
+      </SidebarDropdownWrapper>
 
+      <SidebarItem
+        class="text-[#a6b0cf]  hover:bg-[#2a3042] hover:text-white"
+        label="Orders"
+        {spanClass}
+        icon={{ name: ViewGrid }}
+      />
+      <SidebarItem
+        class="text-[#a6b0cf]  hover:bg-[#2a3042] hover:text-white"
+        label="Customers"
+        {spanClass}
+        icon={{ name: InboxIn }}
+      />
 
-        <button on:click={handleLogout}>Logout</button>
-    </nav>
-</header>
-
-<h3>Welcome {$user.user.firstName}</h3>
+      <SidebarDropdownWrapper
+        class="text-[#a6b0cf]  hover:bg-[#2a3042] hover:text-white"
+        label="CMS"
+        icon={{ name: ShoppingCart }}
+      >
+        <SidebarDropdownItem
+          class="text-[#a6b0cf]  hover:bg-[#2a3042] hover:text-white"
+          label="Pages"
+        />
+        <SidebarDropdownItem
+          class="text-[#a6b0cf]  hover:bg-[#2a3042] hover:text-white"
+          label="Categories"
+        />
+        <SidebarDropdownItem
+          class="text-[#a6b0cf]  hover:bg-[#2a3042] hover:text-white"
+          label="Settings"
+        />
+      </SidebarDropdownWrapper>
+      <SidebarItem
+        class="text-[#a6b0cf]  hover:bg-[#2a3042] hover:text-white"
+        label="Settings"
+        icon={{ name: Cog }}
+      />
+      <SidebarItem
+        class="text-[#a6b0cf]  hover:bg-[#2a3042] hover:text-white"
+        label="Logout"
+        icon={{ name: Logout }}
+        on:click={handleLogout}
+      />
+    </SidebarGroup>
+  </SidebarWrapper>
+</Sidebar>
