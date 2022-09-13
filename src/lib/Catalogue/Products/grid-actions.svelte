aae28943a7454d07d728ba1c7a315301c91bc499
Michael Bouclas <mbouclas@gmail.com>
2022-09-13 13:34:12 +0300

    Grid Actions fix

--- src/lib/Catalogue/Products/grid-actions.svelte
+++ src/lib/Catalogue/Products/grid-actions.svelte
@@ -3,7 +3,7 @@
     Dropdown,
     DropdownItem,
     DropdownDivider,
-    DropdownHeader,
+    ToolbarButton,
     Button,
   } from "flowbite-svelte";
   import { ChevronDown } from "svelte-heros-v2";
@@ -26,7 +26,10 @@
   }
 </script>
 
-<Button class="action-button"><ChevronDown>Dropdown button</ChevronDown></Button>
+
+<ToolbarButton class="action-button text-gray-900">
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5"><path stroke-linecap="round" stroke-linejoin="round" d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z" /></svg>
+</ToolbarButton>
 <Dropdown class="w-44" triggeredBy=".action-button">
   {#if active}
     <DropdownItem><span on:click={handleEvent}>Activate</span></DropdownItem>
