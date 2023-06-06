ac8be045fecf48bdd1637c09a451b2c5538ae004
Michael Bouclas <mbouclas@gmail.com>
2023-06-06 10:15:42 +0300

    testing out tables

--- src/lib/Catalogue/Products/ProductList.svelte
+++ src/lib/Catalogue/Products/ProductList.svelte
@@ -9,6 +9,7 @@
   import { RowSelection } from "gridjs/plugins/selection";
   import Drawer from "svelte-drawer-component";
   import LuckyToast from "../../Shared/Toast.svelte";
+  import { Table, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell, Checkbox} from 'flowbite-svelte';
 
   import { Confirm } from "svelte-confirm";
   import Modals from "../../Shared/Modals.svelte";
@@ -272,6 +273,83 @@
     console.log('row ready', row);
   }
 </script>
+<Table>
+  <TableHead>
+    <TableHeadCell class="!p-4">
+      <Checkbox />
+    </TableHeadCell>
+    <TableHeadCell>Product name</TableHeadCell>
+    <TableHeadCell>Color</TableHeadCell>
+    <TableHeadCell>Category</TableHeadCell>
+    <TableHeadCell>ACCESSORIES</TableHeadCell>
+    <TableHeadCell>AVAILABLE</TableHeadCell>
+    <TableHeadCell>Price</TableHeadCell>
+    <TableHeadCell>WEIGHT</TableHeadCell>
+    <TableHeadCell>ACTION</TableHeadCell>
+  </TableHead>
+  <TableBody class="divide-y">
+    <TableBodyRow>
+      <TableBodyCell class="!p-4">
+        <Checkbox />
+      </TableBodyCell>
+      <TableBodyCell>Apple MacBook Pro 17"</TableBodyCell>
+      <TableBodyCell>Sliver</TableBodyCell>
+      <TableBodyCell>Laptop</TableBodyCell>
+      <TableBodyCell>Yes</TableBodyCell>
+      <TableBodyCell>Yes</TableBodyCell>
+      <TableBodyCell>$2999</TableBodyCell>
+      <TableBodyCell>3.0 lb.</TableBodyCell>
+      <TableBodyCell
+        ><a href="/tables" class="font-medium text-blue-600 hover:underline dark:text-blue-500">
+          Edit
+        </a>
+        <a href="/tables" class="font-medium text-red-600 hover:underline dark:text-red-500">
+          Remove
+        </a></TableBodyCell
+      >
+    </TableBodyRow>
+    <TableBodyRow>
+      <TableBodyCell class="!p-4">
+        <Checkbox />
+      </TableBodyCell>
+      <TableBodyCell>Microsoft Surface Pro</TableBodyCell>
+      <TableBodyCell>White</TableBodyCell>
+      <TableBodyCell>Laptop PC</TableBodyCell>
+      <TableBodyCell>No</TableBodyCell>
+      <TableBodyCell>Yes</TableBodyCell>
+      <TableBodyCell>$1999</TableBodyCell>
+      <TableBodyCell>1.0 lb.</TableBodyCell>
+      <TableBodyCell
+        ><a href="/tables" class="font-medium text-blue-600 hover:underline dark:text-blue-500">
+          Edit
+        </a>
+        <a href="/tables" class="font-medium text-red-600 hover:underline dark:text-red-500">
+          Remove
+        </a></TableBodyCell
+      >
+    </TableBodyRow>
+    <TableBodyRow>
+      <TableBodyCell class="!p-4">
+        <Checkbox />
+      </TableBodyCell>
+      <TableBodyCell>Magic Mouse 2</TableBodyCell>
+      <TableBodyCell>Black</TableBodyCell>
+      <TableBodyCell>Accessories</TableBodyCell>
+      <TableBodyCell>Yes</TableBodyCell>
+      <TableBodyCell>No</TableBodyCell>
+      <TableBodyCell>$99</TableBodyCell>
+      <TableBodyCell>0.2 lb.</TableBodyCell>
+      <TableBodyCell
+        ><a href="/tables" class="font-medium text-blue-600 hover:underline dark:text-blue-500">
+          Edit
+        </a>
+        <a href="/tables" class="font-medium text-red-600 hover:underline dark:text-red-500">
+          Remove
+        </a></TableBodyCell
+      >
+    </TableBodyRow>
+  </TableBody>
+</Table>
 
 <Modals />
 
