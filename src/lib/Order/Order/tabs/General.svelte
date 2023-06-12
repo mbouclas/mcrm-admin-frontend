430477d5d6391c02debb674fc287dcb484b7e00e
Djole <0xdjole@gmail.com>
2023-06-12 18:46:04 +0200

    clear order view

--- src/lib/Order/Order/tabs/General.svelte
+++ src/lib/Order/Order/tabs/General.svelte
@@ -12,6 +12,8 @@
   };
 
   let statusIndex = 0;
+  let shippingAddress;
+  let billingAddress;
   $: {
     if (fields && fields.length) {
       statusIndex = fields.findIndex((field) => field.varName === "status");
@@ -26,189 +28,210 @@
     console.log("change");
   };
 
-  $: console.log(model);
+  $: if (model) {
+    console.log(model);
+    shippingAddress = model.address.find((address) =>
+      address.type.includes("shipping")
+    );
+    billingAddress = model.address.find((address) =>
+      address.type.includes("billing")
+    );
+  }
 </script>
 
 {#if !model}
   <Loading />
 {:else}
   <div class="container">
-    <div class="section">
-      <div class="section-header">General Information</div>
-      <div class="section-body">
-        <div class="field field-general">
-          <span class="field-name">Uuid</span>
-          <span class="field-value">{model.uuid}</span>
-        </div>
+    <div class="layout">
+      <div class="main-content">
+        <!-- Put all content except the customer information here -->
 
-        <div class="field field-general">
-          <span class="field-name">Order Id</span>
-          <span class="field-value">{model.orderId}</span>
-        </div>
+        <div class="section">
+          <div class="section-header">General Information</div>
+          <div class="section-body">
+            <div class="field field-general">
+              <span class="field-name">Uuid</span>
+              <span class="field-value">{model.uuid}</span>
+            </div>
 
-        <div class="field field-general">
-          <span class="field-name">Status</span>
-          <span class="field-value">{model.status}</span>
-        </div>
+            <div class="field field-general">
+              <span class="field-name">Order Id</span>
+              <span class="field-value">{model.orderId}</span>
+            </div>
 
-        <div class="field field-general">
-          <span class="field-name">Created At</span>
-          <span class="field-value">{model.createdAt}</span>
-        </div>
+            <div class="field field-general">
+              <span class="field-name">Status</span>
+              <span class="field-value">{model.status}</span>
+            </div>
 
-        <div class="field field-general">
-          <span class="field-name">VAT</span>
-          <span class="field-value">{model.VAT}</span>
-        </div>
-        <div class="field field-general">
-          <span class="field-name">Total</span>
-          <span class="field-value">{model.total}</span>
-        </div>
+            <div class="field field-general">
+              <span class="field-name">Created At</span>
+              <span class="field-value">{model.createdAt}</span>
+            </div>
 
-        {#if model?.metaData?.notes}
-          <div class="field field-general">
-            <span class="field-name">Notes</span>
-            <span class="field-value">{model.metaData.notes}</span>
+            <div class="field field-general">
+              <span class="field-name">VAT</span>
+              <span class="field-value">{model.VAT}</span>
+            </div>
+            <div class="field field-general">
+              <span class="field-name">Total</span>
+              <span class="field-value">{model.total}</span>
+            </div>
+
+            {#if model?.metaData?.notes}
+              <div class="field field-general">
+                <span class="field-name">Notes</span>
+                <span class="field-value">{model.metaData.notes}</span>
+              </div>
+            {/if}
+
+            {#if model?.metaData?.preferredDeliveryDate}
+              <div class="field field-general">
+                <span class="field-name">Preferred Delivery Date</span>
+                <span class="field-value"
+                  >{model.metaData.preferredDeliveryDate}</span
+                >
+              </div>
+            {/if}
           </div>
-        {/if}
+        </div>
 
-        {#if model?.metaData?.preferredDeliveryDate}
-          <div class="field field-general">
-            <span class="field-name">Preferred Delivery Date</span>
-            <span class="field-value"
-              >{model.metaData.preferredDeliveryDate}</span
-            >
+        <div class="section">
+          <div class="section-body">
+            <div class="cart-section">
+              <div class="section-header">Items</div>
+              <div class="section-body">
+                {#each model.metaData.cart.items as item (item.uuid)}
+                  <div class="cart-item">
+                    <div class="item-details">
+                      <div class="field">
+                        <span class="field-name">Product Title</span>
+                        <span class="field-value">{item.title}</span>
+                      </div>
+                      <div class="field">
+                        <span class="field-name">Quantity</span>
+                        <span class="field-value">{item.quantity}</span>
+                      </div>
+                      <div class="field">
+                        <span class="field-name">Price</span>
+                        <span class="field-value">${item.price}</span>
+                      </div>
+                      <div class="field">
+                        <span class="field-name">Color</span>
+                        <span class="field-value"
+                          >{item.metaData.color.name}</span
+                        >
+                      </div>
+                    </div>
+                    <div class="item-image-wrapper">
+                      <img
+                        src={item.thumb}
+                        alt={item.title}
+                        class="item-image"
+                      />
+                    </div>
+                  </div>
+                {/each}
+              </div>
+            </div>
           </div>
-        {/if}
+        </div>
       </div>
-    </div>
 
-    <div class="section">
-      <div class="section-header">Customer Information</div>
-      <div class="section-body">
-        <div class="field field-general">
-          <span class="field-name">Name</span>
-          <span class="field-value"
-            >{model.user.firstName}, {model.user.lastName}</span
-          >
-        </div>
-        <div class="field field-general">
-          <span class="field-name">Email</span>
-          <span class="field-value">{model.user.email}</span>
-        </div>
-      </div>
-    </div>
-
-    <div class="section">
-      <div class="section-header">Addresses</div>
-      <div class="section-body">
-        {#each model.address as address}
-          <div class="address">
-            <div class="field field-address">
-              <span class="field-name">First Name</span>
-              <span class="field-value">{address.firstName}</span>
-            </div>
-            <div class="field field-address">
-              <span class="field-name">Last Name</span>
-              <span class="field-value">{address.lastName}</span>
-            </div>
-            <div class="field field-address">
-              <span class="field-name">Street</span>
-              <span class="field-value">{address.street}</span>
-            </div>
-            <div class="field field-address">
-              <span class="field-name">Apartment</span>
-              <span class="field-value">{address.apartment}</span>
-            </div>
-            <div class="field field-address">
-              <span class="field-name">City</span>
-              <span class="field-value">{address.city}</span>
-            </div>
-            <div class="field field-address">
-              <span class="field-name">Region</span>
-              <span class="field-value">{address.region}</span>
-            </div>
-            <div class="field field-address">
-              <span class="field-name">Post Code</span>
-              <span class="field-value">{address.postCode}</span>
-            </div>
-            <div class="field field-address">
-              <span class="field-name">Country</span>
-              <span class="field-value">{address.country}</span>
-            </div>
-            <div class="field field-address">
-              <span class="field-name">Phone</span>
-              <span class="field-value">{address.phone}</span>
-            </div>
-          </div>
-        {/each}
-      </div>
-    </div>
-
-    <div class="section">
-      <div class="sub-sections">
-        <div class="sub-section">
-          <div class="sub-section-header">Payment Method</div>
-          <div class="sub-section-body">
-            <div class="field">
-              <span class="field-name">Method</span>
-              <span class="field-value">{model.paymentMethod.title}</span>
-            </div>
-            <div class="field">
-              <span class="field-name">Description</span>
-              <span class="field-value">{model.paymentMethod.description}</span>
-            </div>
-          </div>
-        </div>
-
-        <div class="sub-section">
-          <div class="sub-section-header">Shipping Method</div>
-          <div class="sub-section-body">
-            <div class="field">
-              <span class="field-name">Method</span>
-              <span class="field-value">{model.shippingMethod.title}</span>
-            </div>
-            <div class="field">
-              <span class="field-name">Shipping Time</span>
+      <div class="sidebar">
+        <!-- Move the customer information section here -->
+        <div class="section">
+          <div class="side-section-header">Customer Information</div>
+          <div class="section-body">
+            <div class="field sidebar-field">
               <span class="field-value"
-                >{model.shippingMethod.shippingTime}</span
+                >{model.user.firstName}, {model.user.lastName}</span
               >
             </div>
+            <div class="field sidebar-field">
+              <span class="field-value">{model.user.email}</span>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
 
-    <div class="section">
-      <div class="section-body">
-        <div class="cart-section">
-          <div class="section-header">Items</div>
+        <div class="section">
+          <div class="side-section-header">Shipping address</div>
           <div class="section-body">
-            {#each model.metaData.cart.items as item (item.uuid)}
-              <div class="cart-item">
-                <div class="item-details">
-                  <div class="field">
-                    <span class="field-name">Product Title</span>
-                    <span class="field-value">{item.title}</span>
-                  </div>
-                  <div class="field">
-                    <span class="field-name">Quantity</span>
-                    <span class="field-value">{item.quantity}</span>
-                  </div>
-                  <div class="field">
-                    <span class="field-name">Price</span>
-                    <span class="field-value">${item.price}</span>
-                  </div>
-                  <div class="field">
-                    <span class="field-name">Color</span>
-                    <span class="field-value">{item.metaData.color.name}</span>
-                  </div>
+            <div class="address">
+              <div class="field sidebar-field">
+                <span class="field-value"
+                  >{shippingAddress.firstName}, {shippingAddress.lastName}, {shippingAddress.phone}</span
+                >
+              </div>
+
+              <div class="field sidebar-field">
+                <span class="field-value"
+                  >{billingAddress.country}, {billingAddress.city},{billingAddress.region},
+                  {billingAddress.postCode}</span
+                >
+              </div>
+
+              <div class="field sidebar-field">
+                <span class="field-value"
+                  >{shippingAddress.street}, {shippingAddress.apartment}</span
+                >
+              </div>
+            </div>
+          </div>
+
+          <div class="section">
+            <div class="side-section-header">Billing address</div>
+            <div class="section-body">
+              <div class="address">
+                <div class="field sidebar-field">
+                  <span class="field-value"
+                    >{shippingAddress.firstName}, {shippingAddress.lastName}, {shippingAddress.phone}</span
+                  >
                 </div>
-                <div class="item-image-wrapper">
-                  <img src={item.thumb} alt={item.title} class="item-image" />
+
+                <div class="field sidebar-field">
+                  <span class="field-value"
+                    >{billingAddress.country}, {billingAddress.city}, {billingAddress.region},
+                    {billingAddress.postCode}</span
+                  >
+                </div>
+
+                <div class="field sidebar-field">
+                  <span class="field-value"
+                    >{shippingAddress.street}, {shippingAddress.apartment}</span
+                  >
                 </div>
               </div>
-            {/each}
+            </div>
+
+            <div class="section">
+              <div class="side-section-header">Payment Method</div>
+              <div class="section-body">
+                <div class="field sidebar-field">
+                  <span class="field-value">{model.paymentMethod.title}</span>
+                </div>
+                <div class="field sidebar-field">
+                  <span class="field-value"
+                    >{model.paymentMethod.description}</span
+                  >
+                </div>
+              </div>
+            </div>
+
+            <div class="section">
+              <div class="side-section-header">Shipping Method</div>
+              <div class="section-body">
+                <div class="field sidebar-field">
+                  <span class="field-value">{model.shippingMethod.title}</span>
+                </div>
+                <div class="field sidebar-field">
+                  <span class="field-value"
+                    >{model.shippingMethod.shippingTime}</span
+                  >
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -218,18 +241,38 @@
 
 <style>
   .container {
-    width: 95%;
+    max-width: 1200px;
     margin: 50px auto;
-    border-radius: 12px;
-    background: linear-gradient(90deg, #666666, #323232);
-
+    position: relative; /* Add this to position the sidebar relative to the container */
     padding: 30px;
     box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
-    overflow: hidden;
   }
 
+  .layout {
+    display: flex;
+  }
+
+  .main-content {
+    flex: 3;
+    margin-right: 20px;
+  }
+
+  .sidebar {
+    flex: 1;
+    position: sticky; /* Changed to sticky */
+    top: 0;
+    background: linear-gradient(145deg, #f9f9f9, #ececec);
+    padding: 20px;
+    box-shadow: -2px 0 10px rgba(0, 0, 0, 0.1);
+    height: 100vh;
+    overflow-y: auto;
+    border-radius: 8px;
+  }
+
+  /* Other existing styles remain unchanged */
+
   .section-header {
-    font-size: 24px;
+    font-size: 20px;
     font-weight: 600;
     margin-bottom: 20px;
     color: #111;
@@ -240,8 +283,21 @@
     letter-spacing: 1px;
   }
 
+  .side-section-header {
+    font-size: 16px;
+    font-weight: 600;
+    margin-bottom: 10px;
+    color: white;
+    background: linear-gradient(145deg, #cdcdcd, #444);
+
+    padding: 10px;
+    border-radius: 8px;
+    text-align: center;
+    letter-spacing: 1px;
+  }
+
   .section {
-    padding: 10px 50px;
+    padding: 5px;
     max-width: 1200px;
     margin: auto;
   }
@@ -256,13 +312,13 @@
 
   .field-general {
     padding: 10px;
-    margin-bottom: 20px;
+    margin-bottom: 10px;
   }
 
   .field-name {
     color: #333;
     font-weight: 600;
-    font-size: 16px;
+    font-size: 12px;
     text-align: right;
     width: 150px;
     text-transform: uppercase;
@@ -272,7 +328,7 @@
   .field-value {
     color: #111;
     font-weight: 400;
-    font-size: 16px;
+    font-size: 12px;
     flex-grow: 1;
     padding-left: 20px;
     text-align: left;
@@ -340,7 +396,8 @@
     padding: 0 10px;
   }
 
-  .field-address {
-    margin-bottom: 15px;
+  .sidebar-field {
+    margin-bottom: 2px;
+    background: transparent;
   }
 </style>
