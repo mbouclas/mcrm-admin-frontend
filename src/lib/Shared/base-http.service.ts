6d7e6c6f86f2781648befc32fd1e20c5dfb086b1
Djole <0xdjole@gmail.com>
2023-06-21 21:24:00 +0200

    fix if api check

--- src/lib/Shared/base-http.service.ts
+++ src/lib/Shared/base-http.service.ts
@@ -1,6 +1,6 @@
-import type { IGenericObject } from "./models/generic";
-import { AuthService } from "../Auth/auth.service";
-import queryString from "query-string";
+import type { IGenericObject } from './models/generic';
+import { AuthService } from '../Auth/auth.service';
+import queryString from 'query-string';
 
 export class BaseHttpService {
   protected apiUrl = import.meta.env.VITE_API_URL;
@@ -11,9 +11,9 @@ export class BaseHttpService {
 
   getAuthHeaders() {
     const headers = new Headers();
-    headers.append("Authorization", `Bearer ${AuthService.token()}`);
-    headers.append("credentials", "same-origin");
-    headers.append("x-sess-id", AuthService.getSessionId());
+    headers.append('Authorization', `Bearer ${AuthService.token()}`);
+    headers.append('credentials', 'same-origin');
+    headers.append('x-sess-id', AuthService.getSessionId());
 
     return headers;
   }
@@ -21,19 +21,12 @@ export class BaseHttpService {
   async get(url: string, queryParams?: IGenericObject) {
     const headers = this.getAuthHeaders();
 
-    const query =
-      queryParams && Object.keys(queryParams).length > 0
-        ? `?${this.objectToQueryParams(queryParams)}`
-        : "";
+    const query = queryParams && Object.keys(queryParams).length > 0 ? `?${this.objectToQueryParams(queryParams)}` : '';
     const res = await fetch(`${this.apiUrl}${url}${query}`, { headers });
     return await res.json();
   }
 
-  async post(
-    url: string,
-    body: IGenericObject = {},
-    extraHeaders: IGenericObject = {}
-  ) {
+  async post(url: string, body: IGenericObject = {}, extraHeaders: IGenericObject = {}) {
     try {
       const headers = this.getAuthHeaders();
 
@@ -41,21 +34,21 @@ export class BaseHttpService {
         headers.append(header, extraHeaders[header]);
       });
 
-      const contentType = headers.get("Content-Type");
+      const contentType = headers.get('Content-Type');
       if (!contentType) {
-        headers.append("Content-Type", "application/json");
+        headers.append('Content-Type', 'application/json');
       }
 
       const rawResponse = await fetch(`${this.apiUrl}${url}`, {
-        method: "POST",
+        method: 'POST',
         headers,
         body: JSON.stringify(body),
       });
       if (!rawResponse.ok) {
-        throw new Error("Bad request");
+        throw new Error('Bad request');
       }
       const res = await rawResponse.json();
-      if (typeof res.success !== undefined && !res.success) {
+      if (typeof res.success !== 'undefined' && !res.success) {
         throw res;
       }
       return res;
@@ -64,11 +57,7 @@ export class BaseHttpService {
     }
   }
 
-  async patch(
-    url: string,
-    body: IGenericObject = {},
-    extraHeaders: IGenericObject = {}
-  ) {
+  async patch(url: string, body: IGenericObject = {}, extraHeaders: IGenericObject = {}) {
     try {
       const headers = this.getAuthHeaders();
 
@@ -76,21 +65,21 @@ export class BaseHttpService {
         headers.append(header, extraHeaders[header]);
       });
 
-      const contentType = headers.get("Content-Type");
+      const contentType = headers.get('Content-Type');
       if (!contentType) {
-        headers.append("Content-Type", "application/json");
+        headers.append('Content-Type', 'application/json');
       }
 
       const rawResponse = await fetch(`${this.apiUrl}${url}`, {
-        method: "PATCH",
+        method: 'PATCH',
         headers,
         body: JSON.stringify(body),
       });
       if (!rawResponse.ok) {
-        throw new Error("Bad request");
+        throw new Error('Bad request');
       }
       const res = await rawResponse.json();
-      if (typeof res.success !== undefined && !res.success) {
+      if (typeof res.success !== 'undefined' && !res.success) {
         throw res;
       }
       return res;
@@ -102,23 +91,23 @@ export class BaseHttpService {
   async delete(url: string) {
     try {
       const headers = new Headers();
-      headers.append("Authorization", `Bearer ${AuthService.token()}`);
-      headers.append("x-sess-id", AuthService.getSessionId());
+      headers.append('Authorization', `Bearer ${AuthService.token()}`);
+      headers.append('x-sess-id', AuthService.getSessionId());
 
-      const contentType = headers.get("Content-Type");
+      const contentType = headers.get('Content-Type');
       if (!contentType) {
-        headers.append("Content-Type", "application/json");
+        headers.append('Content-Type', 'application/json');
       }
 
       const rawResponse = await fetch(`${this.apiUrl}${url}`, {
-        method: "DELETE",
+        method: 'DELETE',
         headers,
       });
       if (!rawResponse.ok) {
-        throw new Error("Bad request");
+        throw new Error('Bad request');
       }
       const res = await rawResponse.json();
-      if (typeof res.success !== undefined && !res.success) {
+      if (typeof res.success !== 'undefined' && !res.success) {
         throw res;
       }
       return res;
@@ -129,13 +118,8 @@ export class BaseHttpService {
 
   private objectToQueryParams(obj: IGenericObject) {
     return Object.entries(obj)
-      .map(
-        ([k, v]) =>
-          `${k}=${encodeURIComponent(
-            typeof v === "object" ? JSON.stringify(v) : v
-          )}`
-      )
-      .join("&");
+      .map(([k, v]) => `${k}=${encodeURIComponent(typeof v === 'object' ? JSON.stringify(v) : v)}`)
+      .join('&');
   }
 
   getGridPaginationObject(limit = 10) {
@@ -144,9 +128,7 @@ export class BaseHttpService {
       limit,
       server: {
         url: (prev, page, limit) => {
-          return `${prev}${prev.includes("?") ? "&" : "?"}limit=${limit}&page=${
-            page + 1
-          }`;
+          return `${prev}${prev.includes('?') ? '&' : '?'}limit=${limit}&page=${page + 1}`;
         },
       },
     };
@@ -156,25 +138,23 @@ export class BaseHttpService {
     return {
       server: {
         url: (prev, keyword) => {
-          return `${prev}${prev.includes("?") ? "&" : "?"}q=${keyword}`;
+          return `${prev}${prev.includes('?') ? '&' : '?'}q=${keyword}`;
         },
       },
     };
   }
 
-  getGridSortObject(cols = [{ id: "title", idx: 0 }]) {
+  getGridSortObject(cols = [{ id: 'title', idx: 0 }]) {
     return {
       multiColumn: false,
       server: {
         url: (prev, columns) => {
           if (!columns.length) return prev;
           const col = columns[0];
-          const dir = col.direction === 1 ? "asc" : "desc";
+          const dir = col.direction === 1 ? 'asc' : 'desc';
           let colName = cols.find((c) => c.idx === col.index).id;
 
-          return `${prev}${
-            prev.includes("?") ? "&" : "?"
-          }orderBy=${colName}&way=${dir}`;
+          return `${prev}${prev.includes('?') ? '&' : '?'}orderBy=${colName}&way=${dir}`;
         },
       },
     };
@@ -186,7 +166,7 @@ export class BaseHttpService {
       qs = queryString.stringify(filters);
     }
 
-    return `${this.apiUrl}${endpoint}${qs ? `?${qs}` : ""}`;
+    return `${this.apiUrl}${endpoint}${qs ? `?${qs}` : ''}`;
   }
 
   getGridUrl(endpoint: string, filters = {}, thenFunction: Function) {
