df1ebf82f72ff5175d938ab474eb442d69d6edc2
Djole <0xdjole@gmail.com>
2023-06-22 01:44:56 +0200

    remove table

--- src/lib/DynamicFields/Table.svelte
+++ src/lib/DynamicFields/Table.svelte
@@ -1,49 +1,11 @@
 <script>
-  import { TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell, Table, Checkbox } from 'flowbite-svelte';
+  import { Checkbox } from 'flowbite-svelte';
   import { createEventDispatcher } from 'svelte';
 
   export let items = [];
-  export let pagination;
   export let fields;
 
-  let searchTerm = '';
-
-  let activePageName = 1;
   const dispatch = createEventDispatcher();
-
-  const generatePages = (currentPage) => {
-    let between = [];
-
-    if (currentPage !== 1 && currentPage !== pagination.totalPages) {
-      between = [{ name: currentPage }];
-    }
-
-    pages = [{ name: 1 }, ...between, { name: pagination.totalPages }];
-  };
-
-  $: generatePages(pagination.page || 1);
-
-  let pages = [];
-
-  const previous = () => {
-    if (pagination.page > 1) {
-      pagination.page = pagination.page - 1;
-      dispatch('reload', { pagination });
-    }
-  };
-  const next = () => {
-    if (pagination.page < pagination.totalPages) {
-      pagination.page = pagination.page + 1;
-      dispatch('reload', { pagination });
-    }
-  };
-  const handleClick = (page) => {
-    const clickedPage = page.name;
-    if (pagination.page !== clickedPage) {
-      pagination.page = clickedPage;
-      dispatch('reload', { pagination });
-    }
-  };
 
   const handleAction = (actionType, item) => {
     dispatch('action', { actionType, item });
@@ -52,52 +14,44 @@
   $: fields = [{ varName: 'checkbox', label: 'Checkbox' }, ...fields, { varName: 'action', label: 'Actions' }];
 </script>
 
-<Table hoverable={true} bind:inputValue={searchTerm}>
-  <TableHead>
+<div class="flex flex-col w-full">
+  <div class="flex">
     {#each fields as field}
-      {#if field.varName === 'checkbox'}
-        <TableHeadCell class="!p-2 justify-content-center">
+      <div class="flex-1 p-2 border-b border-gray-500 text-center bg-gray-500 font-bold">
+        {#if field.varName === 'checkbox'}
           <Checkbox />
-        </TableHeadCell>
-      {:else}
-        <TableHeadCell>
+        {:else}
           {field.label}
-        </TableHeadCell>
-      {/if}
+        {/if}
+      </div>
     {/each}
-  </TableHead>
-  <TableBody>
-    {#if items && items.length}
-      {#each items as item}
-        <TableBodyRow>
-          {#each fields as field}
+  </div>
+  {#if items && items.length}
+    {#each items as item}
+      <div class="flex">
+        {#each fields as field}
+          <div class="flex-1 p-2 border-b border-gray-500 text-center bg-gray-700">
             {#if field.varName === 'action'}
-              <TableBodyCell
-                ><a
-                  on:click={() => handleAction('edit', item)}
-                  class="font-medium text-blue-600 hover:underline dark:text-blue-500"
-                >
-                  Edit
-                </a>
-                <a
-                  on:click={() => handleAction('delete', item)}
-                  class="font-medium text-red-600 hover:underline dark:text-red-500"
-                >
-                  Delete
-                </a></TableBodyCell
+              <a
+                on:click={() => handleAction('edit', item)}
+                class="font-medium text-blue-600 hover:underline dark:text-blue-500"
               >
+                Edit
+              </a>
+              <a
+                on:click={() => handleAction('delete', item)}
+                class="font-medium text-red-600 hover:underline dark:text-red-500"
+              >
+                Delete
+              </a>
             {:else if field.varName === 'checkbox'}
-              <TableBodyCell class="!p-2 justify-content-center">
-                <Checkbox />
-              </TableBodyCell>
+              <Checkbox />
             {:else}
-              <TableBodyCell
-                >{typeof item[field.varName] !== 'undefined' ? item[field.varName] : 'No value'}</TableBodyCell
-              >
+              {typeof item[field.varName] !== 'undefined' ? item[field.varName] : 'No value'}
             {/if}
-          {/each}
-        </TableBodyRow>
-      {/each}
-    {/if}
-  </TableBody>
-</Table>
+          </div>
+        {/each}
+      </div>
+    {/each}
+  {/if}
+</div>
