2a2b06f740c784bee6c4ac9ed1291cd9ee427bac
Yai9 <arsenovlidija02@gmail.com>
2023-06-24 20:24:37 +0200

    UI fixing

--- src/lib/Catalogue/Properties/PropertiesList.svelte
+++ src/lib/Catalogue/Properties/PropertiesList.svelte
@@ -1,16 +1,16 @@
 <script>
-  import queryString from "query-string";
-  import { useParams, useLocation, useNavigate } from "svelte-navigator";
-  import { PropertiesService } from "../services/properties/properties.service";
-  import { onMount } from "svelte";
-  import ActionList from "./grid-actions.svelte";
-  import Grid from "gridjs-svelte";
-  import { h } from "gridjs";
-  import { RowSelection } from "gridjs/plugins/selection";
-  import Drawer from "svelte-drawer-component";
+  import queryString from 'query-string';
+  import { useParams, useLocation, useNavigate } from 'svelte-navigator';
+  import { PropertiesService } from '../services/properties/properties.service';
+  import { onMount } from 'svelte';
+  import ActionList from './grid-actions.svelte';
+  import Grid from 'gridjs-svelte';
+  import { h } from 'gridjs';
+  import { RowSelection } from 'gridjs/plugins/selection';
+  import Drawer from 'svelte-drawer-component';
 
-  import { Confirm } from "svelte-confirm";
-  import Modals from "../../Shared/Modals.svelte";
+  import { Confirm } from 'svelte-confirm';
+  import Modals from '../../Shared/Modals.svelte';
 
   let openFilter = false;
   let openProductEditModal = false;
@@ -21,7 +21,7 @@
   const location = useLocation();
   const navigate = useNavigate();
   const filters = {
-    with: ["propertyValues"],
+    with: ['propertyValues'],
   };
   let gridInstance;
   $: selectedRows = [];
@@ -45,10 +45,10 @@
       props: { title: `edit ${id}`, id, active },
     });
 
-    e.$on("grid-action", (m) => console.log(m));
-    e.$on("delete-row", (e) => deleteItem(e.detail.id));
-    e.$on("activate-item", (e) => activateRow(e.detail.id));
-    e.$on("de-activate-item", (e) => de_activateRow(e.detail.id));
+    e.$on('grid-action', (m) => console.log(m));
+    e.$on('delete-row', (e) => deleteItem(e.detail.id));
+    e.$on('activate-item', (e) => activateRow(e.detail.id));
+    e.$on('de-activate-item', (e) => de_activateRow(e.detail.id));
   }
 
   const server = service.getGridUrl(filters);
@@ -56,39 +56,37 @@
   const search = service.getGridSearchObject();
   const sort = service.getGridSortObject([
     {
-      id: "title",
+      id: 'title',
       idx: 2,
     },
     {
-      id: "description",
+      id: 'description',
       idx: 3,
     },
     {
-      id: "createdAt",
+      id: 'createdAt',
       idx: 4,
     },
     {
-      id: "active",
+      id: 'active',
       idx: 5,
     },
   ]);
   const columns = [
     {
-      id: "selectRow",
+      id: 'selectRow',
       sort: false,
-      name: h("input", {
-        type: "checkbox",
+      name: h('input', {
+        type: 'checkbox',
         onChange: (e) => {
           allRowsSelected = e.target.checked;
           // Exceptionally hacky. There's no documented method to get the table data and do something with them
           // So we find all the checkboxes on the table and click them
           // There is of course the obvious bug where if there were selected rows, and you click on this
           // only the inverse will happen. This calls for an intermediate action, like on gmail
-          gridInstance.config.tableRef.current.base
-            .querySelectorAll(".gridjs-checkbox")
-            .forEach((checkbox) => {
-              checkbox.click();
-            });
+          gridInstance.config.tableRef.current.base.querySelectorAll('.gridjs-checkbox').forEach((checkbox) => {
+            checkbox.click();
+          });
         },
       }),
       plugin: {
@@ -105,134 +103,133 @@
       },
     },
     {
-      name: "uuid",
-      id: "uuid",
+      name: 'uuid',
+      id: 'uuid',
       hidden: true,
     },
     {
-      name: "Title",
-      id: "title",
+      name: 'Title',
+      id: 'title',
       formatter: (cell, row) => {
         return h(
-          "p",
+          'p',
           {
             // className:
             //   "py-2 mb-4 px-4 border rounded-md text-white bg-blue-600",
             onClick: (e) => {
               e.preventDefault();
-              navigate("/catalogue/properties/" + row.cells[1].data);
+              navigate('/catalogue/properties/' + row.cells[1].data);
             },
           },
-          cell
+          cell,
         );
       },
     },
     {
-      name: "Property Values",
-      id: "propertyValue",
+      name: 'Property Values',
+      id: 'propertyValue',
       formatter: (cell, row) => {
         if (cell && cell.length) {
           const names = cell.map((dataItem) => dataItem.name);
 
-          let firstThree = names.slice(0, 3).join(", ");
-          let remainingItems =
-            names.length > 3 ? ` + ${names.length - 3} more` : "";
+          let firstThree = names.slice(0, 3).join(', ');
+          let remainingItems = names.length > 3 ? ` + ${names.length - 3} more` : '';
 
           let result = firstThree + remainingItems;
 
           return h(
-            "div",
+            'div',
             {
-              style: { display: "flex" },
+              style: { display: 'block' },
             },
             [
               h(
-                "span", // first span, the items
+                'span', // first span, the items
                 {
                   style: {
                     flex: 5,
-                    overflow: "hidden",
-                    "text-overflow": "ellipsis",
+                    overflow: 'hidden',
+                    'text-overflow': 'ellipsis',
                   },
                 },
-                firstThree
+                firstThree,
               ),
               h(
-                "span", // second span, the "+ X more"
+                'span', // second span, the "+ X more"
                 {
                   style: {
                     flex: 1,
                   },
                 },
-                remainingItems
+                remainingItems,
               ),
-            ]
+            ],
           );
         }
 
-        return h("p", {}, "No property values");
+        return h('p', {}, 'No property values');
       },
     },
 
     {
-      name: "Description",
-      id: "description",
+      name: 'Description',
+      id: 'description',
     },
     {
-      name: "Created",
-      id: "createdAt",
+      name: 'Created',
+      id: 'createdAt',
       hidden: true,
       formatter: (cell) => {
-        return new Date(cell).toLocaleString("el-EL", {
-          month: "short",
-          year: "numeric",
-          hour: "numeric",
-          minute: "numeric",
+        return new Date(cell).toLocaleString('el-EL', {
+          month: 'short',
+          year: 'numeric',
+          hour: 'numeric',
+          minute: 'numeric',
         });
       },
     },
     {
-      name: "Updated",
-      id: "updatedAt",
+      name: 'Updated',
+      id: 'updatedAt',
       hidden: true,
       formatter: (cell) => {
-        return new Date(cell).toLocaleString("el-EL", {
-          month: "short",
-          year: "numeric",
-          hour: "numeric",
-          minute: "numeric",
+        return new Date(cell).toLocaleString('el-EL', {
+          month: 'short',
+          year: 'numeric',
+          hour: 'numeric',
+          minute: 'numeric',
         });
       },
     },
     {
-      name: "Active",
-      id: "active",
+      name: 'Active',
+      id: 'active',
       formatter: (cell) => {
-        return cell ? "Yes" : "No";
+        return cell ? 'Yes' : 'No';
       },
       sort: false,
     },
     {
-      name: "Actions",
+      name: 'Actions',
       formatter: (cell, row, idx) => {
         const id = row.cells[1].data;
         const active = row.cells[7].data;
         createActionsButton({ row, active, id });
 
-        return h("div", { id: `action-${row.id}` }, "");
+        return h('div', { id: `action-${row.id}` }, '');
       },
     },
   ];
   const style = {
     table: {
-      "white-space": "nowrap",
+      'white-space': 'nowrap',
     },
   };
 
   onMount(async () => {
-    gridInstance.on("ready", () => {
-      const checkboxPlugin = gridInstance.config.plugin.get("selectRow");
-      checkboxPlugin.props.store.on("updated", (state) => {
+    gridInstance.on('ready', () => {
+      const checkboxPlugin = gridInstance.config.plugin.get('selectRow');
+      checkboxPlugin.props.store.on('updated', (state) => {
         selectedRows = state.rowIds;
         // console.log(selectedRows);
       });
@@ -288,32 +285,13 @@
   <h1 class="mt-4 mb-2 text-lg">Property List</h1>
   <div class="toolbar flex justify-end bg-[#517acd]">
     <div class="p-6">
-      <i
-        class="fa-solid fa-bars-filter text-white cursor-pointer mr-2"
-        on:click={() => (openFilter = true)}
-      />
-      <i
-        class="fa-solid fa-plus text-white cursor-pointer"
-        on:click={() => navigate("/catalogue/properties/new")}
-      />
+      <i class="fa-solid fa-bars-filter text-white cursor-pointer mr-2" on:click={() => (openFilter = true)} />
+      <i class="fa-solid fa-plus text-white cursor-pointer" on:click={() => navigate('/catalogue/properties/new')} />
       {#if Array.isArray(selectedRows) && selectedRows.length > 0}
-        <i
-          class="fa-solid fa-eye text-white cursor-pointer ml-6 mr-2"
-          on:click={() => activateRows()}
-        />
-        <i
-          class="fa-solid fa-eye-slash text-[#9f9f9f] cursor-pointer mr-6"
-          on:click={() => de_activateRows()}
-        />
-        <Confirm
-          confirmTitle="Delete"
-          cancelTitle="Cancel"
-          let:confirm={confirmThis}
-        >
-          <i
-            class="fa-solid fa-trash-can text-[#892626] cursor-pointer"
-            on:click={() => confirmThis(deleteItems)}
-          />
+        <i class="fa-solid fa-eye text-white cursor-pointer ml-6 mr-2" on:click={() => activateRows()} />
+        <i class="fa-solid fa-eye-slash text-[#9f9f9f] cursor-pointer mr-6" on:click={() => de_activateRows()} />
+        <Confirm confirmTitle="Delete" cancelTitle="Cancel" let:confirm={confirmThis}>
+          <i class="fa-solid fa-trash-can text-[#892626] cursor-pointer" on:click={() => confirmThis(deleteItems)} />
           <span slot="title"> Are you sure? </span>
           <span slot="description"> You won't be able to revert this! </span>
         </Confirm>
@@ -322,26 +300,14 @@
   </div>
 
   <div class="grid-filter-drawer">
-    <Drawer
-      open={openFilter}
-      size="300px"
-      placement="right"
-      on:clickAway={() => (openFilter = false)}
-    >
+    <Drawer open={openFilter} size="300px" placement="right" on:clickAway={() => (openFilter = false)}>
       <div class=" w-full h-full bg-[#222736]">
         <div class="flex justify-between p-4 text-white w-full">
           <p>Filters</p>
-          <i
-            class="fa-solid fa-xmark text-xl cursor-pointer"
-            on:click={() => (openFilter = false)}
-          />
+          <i class="fa-solid fa-xmark text-xl cursor-pointer" on:click={() => (openFilter = false)} />
         </div>
         <div class="w-full">
-          <input
-            type="text"
-            placeholder="filter"
-            class="bg-[#222736] w-full grid-filter-input"
-          />
+          <input type="text" placeholder="filter" class="bg-[#222736] w-full grid-filter-input" />
         </div>
       </div>
     </Drawer>
@@ -366,7 +332,7 @@
 </div>
 
 <style global>
-  @import "https://cdn.jsdelivr.net/npm/gridjs/dist/theme/mermaid.min.css";
+  @import 'https://cdn.jsdelivr.net/npm/gridjs/dist/theme/mermaid.min.css';
   td,
   th {
     color: #a6b0cf !important;
@@ -398,24 +364,24 @@
     border: 1px solid #32394e !important;
   }
 
-  th[data-column-id="selectRow"] {
+  th[data-column-id='selectRow'] {
     text-align: center;
     border-bottom: 2px solid #32394e !important;
   }
-  th[data-column-id="actions"],
-  td[data-column-id="actions"] {
+  th[data-column-id='actions'],
+  td[data-column-id='actions'] {
     text-align: center;
   }
-  td[data-column-id="actions"] div[data-testid="tooltip"] {
+  td[data-column-id='actions'] div[data-testid='tooltip'] {
     right: 0;
   }
-  td[data-column-id="actions"] div[role="tooltip"] ul {
+  td[data-column-id='actions'] div[role='tooltip'] ul {
     background-color: #222736 !important;
   }
-  td[data-column-id="actions"] div[role="tooltip"] ul > div {
+  td[data-column-id='actions'] div[role='tooltip'] ul > div {
     background-color: #2e3446 !important;
   }
-  td[data-column-id="actions"] div[role="tooltip"] li > div {
+  td[data-column-id='actions'] div[role='tooltip'] li > div {
     color: white;
     display: flex;
     align-items: center;
@@ -423,10 +389,10 @@
     padding-right: 20px;
     opacity: 0.8 !important;
   }
-  td[data-column-id="actions"] div[role="tooltip"] li:hover {
+  td[data-column-id='actions'] div[role='tooltip'] li:hover {
     background-color: #222736 !important;
   }
-  td[data-column-id="actions"] div[role="tooltip"] li > div:hover {
+  td[data-column-id='actions'] div[role='tooltip'] li > div:hover {
     opacity: 1 !important;
   }
 
@@ -439,24 +405,24 @@
     background-color: #2e3446 !important;
     color: #6b7280 !important;
   }
-  td[data-column-id="actions"] button {
+  td[data-column-id='actions'] button {
     margin: auto;
     padding: 5px 15px;
     font-size: 12px;
     border: 1px solid #556ee6;
     background-color: #556ee6;
   }
-  td[data-column-id="actions"] button:hover {
+  td[data-column-id='actions'] button:hover {
     background-color: #485ec4;
   }
-  td[data-column-id="actions"] button:focus {
+  td[data-column-id='actions'] button:focus {
     box-shadow: 0 0 0 0.15rem rgb(111 132 234 / 50%);
   }
   .gridjs-checkbox:not(:checked) {
     background-color: #9daad1;
   }
 
-  .gridjs-th-content input[type="checkbox"]:not(:checked) {
+  .gridjs-th-content input[type='checkbox']:not(:checked) {
     background-color: #9daad1;
   }
   .gridjs-wrapper {
@@ -507,7 +473,7 @@
     background: none !important;
   }
 
-  td[data-column-id="propertyValue"] {
+  td[data-column-id='propertyValue'] {
     width: 300px !important;
     max-width: 300px !important;
   }
--- src/lib/DynamicFields/Table.svelte
+++ src/lib/DynamicFields/Table.svelte
@@ -1,6 +1,7 @@
 <script>
-  import { Checkbox } from 'flowbite-svelte';
-  import { createEventDispatcher } from 'svelte';
+  import { Checkbox, Button } from 'flowbite-svelte';
+  import { createEventDispatcher, onMount } from 'svelte';
+  import * as Icon from 'svelte-heros-v2';
 
   export let items = [];
   export let fields;
@@ -39,23 +40,33 @@
              ${field.varName === 'checkbox' ? 'flex-none w-20' : ''} 
              ${field.varName === 'action' ? 'flex-none w-40' : ''}`}
           >
+            <!-- svelte-ignore a11y-missing-attribute -->
             {#if field.varName === 'action'}
-              <a
+              <!-- svelte-ignore a11y-click-events-have-key-events -->
+              <!-- svelte-ignore a11y-missing-attribute -->
+              <Button
                 on:click={() => handleAction('edit', item)}
-                class="font-medium text-blue-600 hover:underline dark:text-blue-500 cursor-pointer"
+                class="font-medium bg-blue-600 text-white rounded p-2 dark:bg-blue-500"
               >
                 Edit
-              </a>
-              <a
+              </Button>
+              <!-- svelte-ignore a11y-click-events-have-key-events -->
+              <Button
                 on:click={() => handleAction('delete', item)}
-                class="font-medium text-red-600 hover:underline dark:text-red-500 cursor-pointer"
+                class="font-medium bg-red-600 text-white rounded p-2 dark:bg-red-500"
               >
                 Delete
-              </a>
+              </Button>
             {:else if field.varName === 'checkbox'}
               <Checkbox />
+            {:else if typeof item[field.varName] !== 'undefined'}
+              {#if field.varName === 'image'}
+                <img src={item[field.varName]} alt={`image${index}`} width="100" height="100" />
+              {:else}
+                {item[field.varName]}
+              {/if}
             {:else}
-              {typeof item[field.varName] !== 'undefined' ? item[field.varName] : 'No value'}
+              <Icon.XCircle size="40" color="crimson" />
             {/if}
           </div>
         {/each}
