ffe07e528cec3c3639c6c93851a33b59bcebe1ab
Djole <0xdjole@gmail.com>
2023-09-05 01:40:30 +0200

    fields not required by default

--- src/lib/DynamicFields/fields/number-input.svelte
+++ src/lib/DynamicFields/fields/number-input.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
-  import { Input, Label, Helper } from "flowbite-svelte";
-  import type { IDynamicFieldConfigBlueprint } from "../types";
+  import { Input, Label, Helper } from 'flowbite-svelte';
+  import type { IDynamicFieldConfigBlueprint } from '../types';
 
-  export let field: IDynamicFieldConfigBlueprint =
-    {} as IDynamicFieldConfigBlueprint;
+  export let field: IDynamicFieldConfigBlueprint = {} as IDynamicFieldConfigBlueprint;
   export let onChange;
   export let model;
   export let placeholder;
@@ -11,48 +10,43 @@
   export let helperText;
   export let inputId: string;
   export let classes: string[] = [];
-  export let step = field.type === "float" ? 0.01 : 1;
+  export let step = field.type === 'float' ? 0.01 : 1;
   let pristine = true;
-  let value = field.value || "";
+  let value = field.value || '';
 
   let hasError = false;
 
   let onValueChange = (key, value) => {
-    if (value === "") {
+    if (value === '' && field.required) {
       hasError = true;
-      helperText = "This field is required";
-    } else if (
-      parseFloat(value) < 0 &&
-      ["weight", "quantity", "price"].indexOf(model)
-    ) {
+      helperText = 'This field is required';
+    } else if (parseFloat(value) < 0 && ['weight', 'quantity', 'price'].indexOf(model)) {
       hasError = true;
-      helperText = "Should be positive value";
+      helperText = 'Should be positive value';
     } else {
       hasError = false;
       helperText = null;
     }
     pristine = false;
-    if (typeof onChange === "function") {
+    if (typeof onChange === 'function') {
       onChange(key, value);
     }
   };
-
-
 </script>
 
 <div class="mb-6">
   {#if field.label}
-    <Label class={`block mb-2 !text-gray-400 ${hasError ? "has-error" : ""}`}>
+    <Label class={`block mb-2 !text-gray-400 ${hasError ? 'has-error' : ''}`}>
       {field.label}
     </Label>
   {/if}
 
-  <div class={`dynamic-field ${hasError ? "has-error" : ""}`}>
+  <div class={`dynamic-field ${hasError ? 'has-error' : ''}`}>
     <Input
       type="number"
       bind:value={model}
       placeholder={field.placeholder}
-      step={step}
+      {step}
       autoCompleteKey="name"
       on:blur={(e) => {
         onValueChange(field.varName, e.currentTarget.value);
@@ -63,7 +57,7 @@
     />
   </div>
   {#if helperText}
-    <Helper class={hasError ? "helper-text has-error" : "helper-text"}>
+    <Helper class={hasError ? 'helper-text has-error' : 'helper-text'}>
       {helperText}
     </Helper>
   {/if}
