5d86b48e824b547a075603414d56a262f1aa9739
Djole <0xdjole@gmail.com>
2023-09-09 00:36:10 +0200

    here we go

--- src/lib/Catalogue/Properties/tabs/PropertyValues.svelte
+++ src/lib/Catalogue/Properties/tabs/PropertyValues.svelte
@@ -18,6 +18,7 @@
 
   let isPropertyValueModalOpen = false;
   let isDeletePropertyValueModalOpen = false;
+  let isEditPropertyValueModalOpen = false;
   const service = new PropertiesService();
   let showModal = false;
   let searchVal = '';
@@ -164,7 +165,6 @@
   }
 
   const confirmDeletePropertyValue = async () => {
-    console.log(propertyValueData.uuid);
     await service.deletePropertyValue({ propertyUuid, propertyValueUuid: propertyValueData.uuid });
     isDeletePropertyValueModalOpen = false;
     propertyValueData = null;
@@ -174,6 +174,22 @@
   const deletePropertyValue = async (item) => {
     propertyValueData = item;
     isDeletePropertyValueModalOpen = true;
+  };
+
+  const confirmPropertyValueEdit = async () => {
+    await service.updatePropertyValue({
+      ...propertyValueData,
+      uuid: propertyUuid,
+      propertyValueUuid: propertyValueData.uuid,
+    });
+    isEditPropertyValueModalOpen = false;
+    propertyValueData = propertyValueDefault;
+    await search();
+  };
+
+  const cancelPropertyValueEdit = () => {
+    propertyValueData = propertyValueDefault;
+    isEditPropertyValueModalOpen = false;
   };
 </script>
 
@@ -209,7 +225,7 @@
 </Modal>
 
 <Modal bind:showModal={isPropertyValueModalOpen}>
-  <h2 class="flowbite-modal-title text-xl font-bold" slot="header">Add new propertyValue</h2>
+  <h2 class="flowbite-modal-title text-xl font-bold" slot="header">Add new property value</h2>
   <div class="mt-4" slot="content">
     <div class="mb-4">
       <Input label="Name" bind:errors={propertyValueStatus.name.errors} bind:value={propertyValueData.name} required />
@@ -232,6 +248,33 @@
   <div slot="footer">
     <Button disabled={hasPropertyValueErrors} on:click={confirmAddPropertyValueModal}>Create</Button>
     <Button color="alternative" on:click={cancelAddPropertyValueModal}>Cancel</Button>
+  </div>
+</Modal>
+
+<Modal bind:showModal={isEditPropertyValueModalOpen}>
+  <h2 class="flowbite-modal-title text-xl font-bold" slot="header">Edit property value</h2>
+  <div class="mt-4" slot="content">
+    <div class="mb-4">
+      <Input label="Name" bind:errors={propertyValueStatus.name.errors} bind:value={propertyValueData.name} required />
+    </div>
+
+    <div class="mb-4">
+      <Input label="Icon" bind:errors={propertyValueStatus.icon.errors} bind:value={propertyValueData.icon} required />
+    </div>
+
+    <div class="mb-4">
+      <Input
+        label="Image"
+        bind:errors={propertyValueStatus.image.errors}
+        bind:value={propertyValueData.image}
+        required
+      />
+    </div>
+  </div>
+
+  <div slot="footer">
+    <Button disabled={hasPropertyValueErrors} on:click={confirmPropertyValueEdit}>Edit</Button>
+    <Button color="alternative" on:click={cancelPropertyValueEdit}>Cancel</Button>
   </div>
 </Modal>
 <div
@@ -340,9 +383,55 @@
                 </td>
 
                 <td class="px-4 py-4 text-sm whitespace-nowrap">
-                  <button on:click={() => deletePropertyValue(item)} class="text-gray-500"
-                    ><Trash color="white" /></button
-                  >
+                  <div class="flex items-center gap-x-6">
+                    <button
+                      title="Edit Property Value"
+                      on:click={() => {
+                        isEditPropertyValueModalOpen = true;
+                        propertyValueData = item;
+                      }}
+                      type="button"
+                      class="text-gray-500 transition-colors duration-200 dark:hover:text-yellow-500 dark:text-gray-300 hover:text-yellow-500 focus:outline-none"
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="w-5 h-5"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+                        />
+                      </svg>
+                    </button>
+
+                    <button
+                      title="Delete Rule"
+                      on:click|preventDefault={() => {
+                        deletePropertyValue(item);
+                      }}
+                      class="text-gray-500 transition-colors duration-200 dark:hover:text-red-500 dark:text-gray-300 hover:text-red-500 focus:outline-none"
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="w-5 h-5"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                        />
+                      </svg>
+                    </button>
+                  </div>
                 </td>
               </tr>
             {/each}
