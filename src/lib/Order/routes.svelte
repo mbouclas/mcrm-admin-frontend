836223b38d0a115b36b07698beb8448cc049e2e0
Djole <0xdjole@gmail.com>
2023-09-23 04:51:56 +0200

    clear order unused components

--- src/lib/Order/routes.svelte
+++ src/lib/Order/routes.svelte
@@ -1,8 +1,7 @@
 <script>
-  import { Router, Route, Link } from "svelte-navigator";
-  // import OrderList from "./Order/OrderList.svelte";
-  import OrderList from "./Order/Orders.svelte";
-  import Order from "./Order/OrderView.svelte";
+  import { Router, Route, Link } from 'svelte-navigator';
+  import OrderList from './Order/Orders.svelte';
+  import Order from './Order/OrderView.svelte';
 </script>
 
 <Router>
